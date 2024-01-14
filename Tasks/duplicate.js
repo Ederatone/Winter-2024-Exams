37a4cf0aec4331c6bed071f1e7c2937e9800977d
Ederatone <vshkarbanw@ukr.net>
2024-01-14 20:42:55 +0200

    added brackets for if

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -2,14 +2,17 @@
 // Return an array without duplicates
 
 const duplicate = (value, N) => {
-  if (N <= 0) return [];
-  else {
-    const res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
-    }
-    return res;
+
+  if (N <= 0) {
+    return [];
   }
+
+  const res = [];
+  for (let i = 0; i < N; i++) {
+    res[i] = value;
+  }
+
+  return res;
 };
 
 module.exports = duplicate;
