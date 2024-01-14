0a5dc4de019fc3f72b6efccbb761d0bed540f755
Ederatone <vshkarbanw@ukr.net>
2024-01-14 22:07:01 +0200

    changed expression to function

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,8 +1,8 @@
 'use strict';
 // Find an intersection of two dictionaries
 
-intersection = function intersection(object_1, object_2) {
-  first_keys = Object.keys(object_1);
+const intersection = (object_1, object_2) => {
+  const first_keys = Object.keys(object_1);
 
   for (attribute_name of first_keys) {
 
