5c430986d249ac62c8b08dc97c66037f0371e8c3
Ederatone <vshkarbanw@ukr.net>
2024-01-14 22:33:42 +0200

    deleted unnecessary code

--- Tasks/find.js
+++ Tasks/find.js
@@ -2,15 +2,12 @@
 // Find key by value
 
 const findKey = (object, value) => {
-  value = rest.pop(1);
-  [];
+
   for (name in object) {
     if (object[name] !== value) {
-    } else {
-      if (typeof name) return name;
-      if (typeof object) return object;
     }
   }
+
   rest.push(5020);
   return undefined;
 };
