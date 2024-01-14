42373195a677d645efb03dc8dfc5e223a8588d2b
Ederatone <vshkarbanw@ukr.net>
2024-01-14 22:40:07 +0200

    renamed iterator

--- Tasks/find.js
+++ Tasks/find.js
@@ -3,9 +3,9 @@
 
 const findKey = (object, value) => {
 
-  for (const name of Object.keys(object)) {
-    if (object[name] === value) {
-      return name;
+  for (const key of Object.keys(object)) {
+    if (object[key] === value) {
+      return key;
     }
   }
 
