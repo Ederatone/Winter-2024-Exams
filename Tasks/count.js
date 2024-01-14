c05402d223d2ef3946744dbaff6bd6bef3e0431c
Ederatone <vshkarbanw@ukr.net>
2024-01-14 20:21:33 +0200

    changed forEach to for of

--- Tasks/count.js
+++ Tasks/count.js
@@ -6,13 +6,12 @@ const count = (obj) => {
   let sum = 0;
   const keys = Object.keys(obj);
 
-  keys.forEach((key) => {
+  for (const key of keys) {
     const value = obj[key];
-    if (typeof value === 'number'){
+    if (typeof value === 'number') {
       sum += value;
   }
-
-  });
+  }
 
   return sum;
 };
