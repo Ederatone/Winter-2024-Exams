72a272e8b8ada3577268ea147a04d4d7f28f7069
Ederatone <vshkarbanw@ukr.net>
2024-01-14 22:24:42 +0200

    added intermediate variable

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,12 +2,17 @@
 // Find an intersection of two dictionaries
 
 const intersection = (object1, object2) => {
+
   const object1Keys = Object.keys(object1);
   const res = {};
 
   for (const key of object1Keys) {
 
-    res[key] = object1[key];
+    const value = object1[key];
+    if (object2[key] === value) {
+      res[key] = value;
+    }
+
   }
   return res;
 };
