c50b5aecc3aca109cc73b0351911907141e001e3
Ederatone <vshkarbanw@ukr.net>
2024-01-14 19:47:18 +0200

    deleted unused code

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -3,17 +3,16 @@
 
 const DISTINCT = (data) => {
   const A = new Set();
-  let w = 0;
+
   data.forEach((a) => {
     if (A.has(a)) {
       delete data[w];
     } else {
       A.add(a);
     }
-    w++;
   });
-  return data.filter
-  (x => typeof x === 'number');
+
+  return data
 };
 
 module.exports = DISTINCT;
