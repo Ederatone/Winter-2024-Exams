05e96a3de3feea4432dcca7cb65ea0700f60e1ae
Ederatone <vshkarbanw@ukr.net>
2024-01-14 18:25:57 +0200

    removed tolowercase

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,11 +1,11 @@
 // Get day number
 
-const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const parseDay = (s) => {
   let i;
   for (i = 0; i < DAYS.length; i++) {
-    if (s.startsWith(DAYS[i].toLowerCase())) {
+    if (s.startsWith(DAYS[i])) {
       return i + 1;
     }
   }
