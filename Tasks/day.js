2a5f0c35c34cebd5d16b3c3ecf6dc853d6852d9e
Ederatone <vshkarbanw@ukr.net>
2024-01-14 18:32:01 +0200

    relocated let i, added 'use strict'

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,10 +1,10 @@
 // Get day number
+'use strict';
 
 const DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const parseDay = (day) => {
-  let i;
-  for (i = 0; i < DAYS.length; i++) {
+  for (let i = 0; i < DAYS.length; i++) {
     if (day.startsWith(DAYS[i])) {
       return i + 1;
     }
